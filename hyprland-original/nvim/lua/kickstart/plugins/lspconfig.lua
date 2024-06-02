c2703f28263c91ace17f31402ee39ff946702a0d
Thomas Phillips <tphillips2020@gmail.com>
2024-06-02 19:20:00 +0930

    Configured lsp servers for Python and C/C++, added Python formatter black

--- hyprland-original/nvim/lua/kickstart/plugins/lspconfig.lua
+++ hyprland-original/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -156,9 +156,11 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {
+          filetypes = { 'c', 'cpp' },
+        },
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -198,6 +200,7 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'black',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
