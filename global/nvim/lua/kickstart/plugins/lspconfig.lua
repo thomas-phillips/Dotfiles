683f9e3a14181a402dfeeab0eda3cdaeb69e064f
Thomas Phillips <tphillips2020@gmail.com>
2024-06-03 19:01:30 +0930

    Added css-lsp server to neovim for lspconfig

--- global/nvim/lua/kickstart/plugins/lspconfig.lua
+++ global/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -162,6 +162,7 @@ return {
         -- gopls = {},
         pyright = {},
         bashls = {},
+        cssls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
