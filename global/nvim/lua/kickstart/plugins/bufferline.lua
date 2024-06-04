1c88189d9fd9e0911b832d0b19a169c881c2e722
Thomas Phillips <tphillips2020@gmail.com>
2024-06-05 00:10:27 +0930

    Added bufferline with proper config
    Buffers can how be chosen with the leader key <leader>bp and <leader>bd

--- global/nvim/lua/kickstart/plugins/bufferline.lua
+++ global/nvim/lua/kickstart/plugins/bufferline.lua
@@ -4,6 +4,14 @@ return {
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
     vim.opt.termguicolors = true
-    require('bufferline').setup {}
+    require('bufferline').setup {
+      options = {
+        'buffers',
+      },
+    }
+
+    local builtin = require 'bufferline'
+    vim.keymap.set('n', '<leader>bp', builtin.pick, { desc = '[P]ick [B]uffer' })
+    vim.keymap.set('n', '<leader>bd', builtin.close_with_pick, { desc = '[D]elete [B]uffer' })
   end,
 }
