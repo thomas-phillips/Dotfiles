1ba955a7761816a7a381570b7f464541fae84efe
Thomas Phillips <tphillips2020@gmail.com>
2024-06-04 22:29:00 +0930

    Cleaned up hyprland with new keybindings and removed bufferline from neovim
    Added vim motion keybindings to hyprland config
    Removed vscode shortcuit
    doubled rofi shortcut
    set hide mouse upon keypress to true

--- global/nvim/lua/lazy-plugins.lua
+++ global/nvim/lua/lazy-plugins.lua
@@ -60,7 +60,7 @@ require('lazy').setup({
   require 'kickstart/plugins/vim-tmux-navigator',
 
   -- [[ Buffer tabs ]]
-  require 'kickstart/plugins/bufferline',
+  -- require 'kickstart/plugins/bufferline',
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
