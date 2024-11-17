df3dd4eca88139caae0e7af9d30a3dbf899ee20f
Thomas Phillips <tphillips2020@gmail.com>
2024-11-17 22:40:15 +1030

    Created universal dotfile directories with current used setup
    Will tag each setup to hopefully be more trackable

---
+++ dotfiles/nvim/lua/plugins/conform.lua
@@ -0,0 +1,23 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			-- Customize or remove this keymap to your liking
+			"<leader>f",
+			function()
+				require("conform").format({ async = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	opts = {
+		-- Define your formatters
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "black" },
+		},
+	},
+}
