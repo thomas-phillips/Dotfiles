903042f60b9acd14087b41eb06dbc6d195ce2d12
Thomas Phillips <tphillips2020@gmail.com>
2024-12-09 23:25:33 +1030

    Updated keymapping for treesj to stop conflicting <leader>m mappings

--- dotfiles/nvim/lua/plugins/treesj.lua
+++ dotfiles/nvim/lua/plugins/treesj.lua
@@ -1,14 +1,10 @@
 return {
 	"Wansmer/treesj",
-	keys = {
-		"<leader>m",
-		"<CMD>TSJToggle<CR>",
-		desc = "Toggle Treesitter Join",
-	},
+	-- keys = {
+	-- },
 	dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
 	config = function()
-		require("treesj").setup({
-			use_default_keymaps = false,
-		})
+		require("treesj").setup({ use_default_keymaps = false })
+		vim.keymap.set("n", "<leader>t", require("treesj").toggle)
 	end,
 }
