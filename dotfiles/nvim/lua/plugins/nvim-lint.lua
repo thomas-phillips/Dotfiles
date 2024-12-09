e510ddcc17ba5381e41a97ca7c21f600145a9d6e
Thomas Phillips <tphillips2020@gmail.com>
2024-12-09 23:25:14 +1030

    Changed linter, gitsigns and harpoon mappings

--- dotfiles/nvim/lua/plugins/nvim-lint.lua
+++ dotfiles/nvim/lua/plugins/nvim-lint.lua
@@ -16,7 +16,7 @@ return {
 			end,
 		})
 
-		vim.keymap.set("n", "<leader>ll", function()
+		vim.keymap.set("n", "<leader>l", function()
 			lint.try_lint()
 		end, { desc = "Trigger linting for current file" })
 	end,
