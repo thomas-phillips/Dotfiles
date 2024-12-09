3c93bf611c709227c6a4f765a2c832edfc77fdf4
Thomas Phillips <tphillips2020@gmail.com>
2024-12-09 23:24:30 +1030

    Reformated, cleaned up and edited which key plugin configuration

--- dotfiles/nvim/lua/plugins/which-key.lua
+++ dotfiles/nvim/lua/plugins/which-key.lua
@@ -1,39 +1,28 @@
--- NOTE: Plugins can also be configured to run Lua code when they are loaded.
---
--- This is often very useful to both group configuration, as well as handle
--- lazy loading plugins that don't need to be loaded immediately at startup.
---
--- For example, in the following configuration, we use:
---  event = 'VimEnter'
---
--- which loads which-key before all the UI elements are loaded. Events can be
--- normal autocommands events (`:help autocmd-events`).
---
--- Then, because we use the `config` key, the configuration only runs
--- after the plugin has been loaded:
---  config = function() ... end
-
 return {
-  { -- Useful plugin to show you pending keybinds.
-    'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
+	{
+		"folke/which-key.nvim",
+		event = "VimEnter", -- Sets the loading event to 'VimEnter'
+		config = function() -- This is the function that runs, AFTER loading
+			require("which-key").setup()
 
-      -- Document existing key chains
-      require('which-key').add {
-        { '<leader>c_', group = '[C]ode' },
-        { '<leader>d_', group = '[D]ocument' },
-        { '<leader>r_', group = '[R]ename' },
-        { '<leader>s_', group = '[S]earch' },
-        { '<leader>w_', group = '[W]orkspace' },
-        { '<leader>t_', group = '[T]oggle' },
-        { '<leader>h_', group = 'Git [H]unk' },
-        { '<leader>b_', group = '[B]uffer' },
-        -- Visual Mode
-        { '<leader>h', group = 'Git [H]unk', mode = 'v' },
-      }
-    end,
-  },
+			-- Document existing key chains
+			require("which-key").add({
+				{ "<leader>s", group = "[S]earch" },
+				{
+					"<leader>b",
+					group = "[B]uffer",
+					expand = function()
+						return require("which-key.extras").expand.buf()
+					end,
+				},
+				{ "<leader>h", group = "[H]arpoon" },
+				{ "<leader>y", group = "[Y]azi" },
+				{ "<leader>g", group = "[G]itsigns" },
+				{ "<leader>gt", group = "[G]itsigns [t]oggle" },
+				{ "<leader>t", group = "[T]reeSJ" },
+				-- Visual Mode
+				{ "<leader>g", group = "Git [H]unk", mode = "v" },
+			})
+		end,
+	},
 }
-
