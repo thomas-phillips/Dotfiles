512c4b8fca661a3a1a222a6f64e69551fafbe6d9
Thomas Phillips <tphillips2020@gmail.com>
2025-03-18 17:18:09 +1030

    Disable features for big files

--- dotfiles/nvim/lua/plugins/bigfile.lua
+++ dotfiles/nvim/lua/plugins/bigfile.lua
@@ -3,6 +3,16 @@ return {
 	config = function()
 		require("bigfile").setup({
 			filesize = 500,
+			features = { -- features to disable
+				"indent_blankline",
+				"illuminate",
+				"lsp",
+				"treesitter",
+				"syntax",
+				"matchparen",
+				"vimopts",
+				"filetype",
+			},
 		})
 	end,
 }
